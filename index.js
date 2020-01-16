b429e05fe99f3c9da183c4120b80208396d9ee85
Edvanderson Lisboa <edvanderson.lisboa@redespc.intra>
2020-01-16 11:19:01 -0300

    comunicação funcionado co api codenation

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 
 const app = express();
+ app.set('view engine', 'ejs');
+ app.set('views','./views');
+app.use(bodyParser.json());
 
-app.listen(3000, ()=>{
-    console.log('servidor rodando na porta 3000');
+
+
+app.get('/', function(req, res){
+    res.render("home");
+})
+
+app.post('/cripitografia',function(req,res){
+    debugger;
+    console.log(req.body);
+    res.json(req.body)
+    
+})
+
+app.listen(300, ()=>{
+    console.log('servidor rodando na porta 300');
 })
